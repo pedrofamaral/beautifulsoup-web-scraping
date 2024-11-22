4e6df462c197a027d0f748d08886703b6aa0204b
Pedro Amaral <142808888+pedrofamaral@users.noreply.github.com>
2024-11-22 18:06:21 -0300

    Update util_beautifulsoup.py

--- util_beautifulsoup.py
+++ util_beautifulsoup.py
@@ -1,1 +1,16 @@
+# Navigate through the siblings 
+def navigate_siblings(header):
+    # Check the next brother
+    next_sibling = header.find_next_sibling()
+    if next_sibling:
+        print('Next sibling of first <h1> tag:', next_sibling)
+    else:
+        print('No next sibling found for the <h1> tag.')
+
+    # Check the previous brother
+    previous_sibling = header.find_previous_sibling()
+    if previous_sibling:
+        print('Previous sibling of first <h1> tag:', previous_sibling)
+    else:
+        print('No previous sibling found for the <h1> tag.')
 
